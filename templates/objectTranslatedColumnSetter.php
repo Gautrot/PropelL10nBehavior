735303bbc1e012c7a4c3f28062fc9b00a04ddd1b
Gautrot <alexandre.gautrot@etik.com>
2026-09-12 18:07:52 +0200

    test: fixed locale set

--- templates/objectTranslatedColumnSetter.php
+++ templates/objectTranslatedColumnSetter.php
@@ -1,12 +1,6 @@
 <?= $comment; ?>
 <?= $functionStatement; ?>
 
-    if ($locale === null) {
-        $locale = $this->getLocale();
-    }
-    if ($locale === null) {
-        $locale = PropelL10n::getLocale();
-    }
     $this->getCurrentTranslation($locale)->set<?= $columnPhpName; ?>(<?= $params; ?>);
 
     return $this;
